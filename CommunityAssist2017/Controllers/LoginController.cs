8ccc87ca3424e125829b0372c6ac31c50ffe8b97
Tran Duong <36751280+tranzilla@users.noreply.github.com>
2018-05-01 13:16:23 -0700

    Add Login Form

---
+++ CommunityAssist2017/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssist2017.Models;
+
+namespace CommunityAssist2017.Controllers
+{
+    public class LoginController : Controller
+    {
+        CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "Email, PlainPassword")]LoginClass lc)
+        {
+            int results = db.usp_Login(lc.Email, lc.PlainPassword);
+            int personkey = 0;
+            Message msg = new Message();
+            if(results != -1)
+            {
+                var pkey = (from r in db.People
+                         where r.PersonEmail.Equals(lc.Email)
+                         select r.PersonKey).FirstOrDefault();
+                personkey = (int)pkey;
+                Session["PersonKey"] = personkey;
+
+                msg.MessageText = "Welcome, " + lc.Email;
+            }
+            else
+            {
+                msg.MessageText = "Invalid Login";
+            }
+            return View("Result", msg);
+        }
+        public ActionResult Reuslt(Message msg)
+        {
+            return View(msg);
+        }
+    }
+}
