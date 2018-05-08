b0da6028327db09c07f2cd4b9c773114e1523b02
Tran Duong <36751280+tranzilla@users.noreply.github.com>
2018-05-08 11:57:16 -0700

    Add Donation Controller

---
+++ CommunityAssist2017/Controllers/DonationController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssist2017.Models;
+
+namespace CommunityAssist2017.Controllers
+{
+    public class DonationController : Controller
+    {
+        // GET: Donation
+        public ActionResult Index()
+        {
+            if (Session["PersonKey"] == null)
+            {
+                //Message m = new Message ("You must Login to Donate");
+                return RedirectToAction("Index", "Login"); //this will redirect the user to the login page if they did not log in
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "Personkey, DonationDate, DonationAmount, DonationConfirmationCode")]Donation d)
+
+        {
+            CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+            d.DonationConfirmationCode = Guid.NewGuid();
+            d.PersonKey=(int)Session["PersonKey"];
+            d.DonationDate=DateTime.Now;
+            db.Donations.Add(d);
+            db.SaveChanges();
+            Message m = new Message("Thank you for your donations!");
+            return View("Result", m);
+    }
+
+    public ActionResult Result(Message msg)
+    {
+        return View(msg);
+    }
+
+}
+    }
